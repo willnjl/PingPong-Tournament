8cdccab47cda4fe87fe28eb86df5d4a21112246c
Will <will_leighton@outlook.com>
2020-09-27 17:47:56 +0100

    Updates

---
+++ precache-manifest.f45c03abbee359c3a531fbc70961a9d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28aaf977c52b02ec7892a1a63ada502",
+    "url": "/PingPong-Tornament/index.html"
+  },
+  {
+    "revision": "30cb5ce09b8d24202af8",
+    "url": "/PingPong-Tornament/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "33365b49b283e9b1c8e6",
+    "url": "/PingPong-Tornament/static/css/main.6d83e7a1.chunk.css"
+  },
+  {
+    "revision": "30cb5ce09b8d24202af8",
+    "url": "/PingPong-Tornament/static/js/2.28d5be70.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PingPong-Tornament/static/js/2.28d5be70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33365b49b283e9b1c8e6",
+    "url": "/PingPong-Tornament/static/js/main.39f9f1c0.chunk.js"
+  },
+  {
+    "revision": "c7e8d3a2bbac4ac4d149",
+    "url": "/PingPong-Tornament/static/js/runtime-main.b56a9dac.js"
+  },
+  {
+    "revision": "7c0f5a809bc040d5e80f4edf68442e03",
+    "url": "/PingPong-Tornament/static/media/racket.7c0f5a80.jpg"
+  }
+]);
